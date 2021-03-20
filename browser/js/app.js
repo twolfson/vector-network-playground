50f89e3d126cf0b59c31662a06461f236cde00c0
Todd Wolfson <todd@twolfson.com>
2021-03-20 16:31:22 -0700

    Added width/height detection

--- browser/js/app.js
+++ browser/js/app.js
@@ -1,10 +1,24 @@
 // Define our core application
 function App(canvasEl) {
+  // Define common properties
   this.canvasEl = canvasEl;
   this.context = canvasEl.getContext('2d');
+  this.width = null;
+  this.height = null;
+
+  // Resolve our width/height
+  this.handleResize();
+
+  // Start our render loop
   this.queueRender();
 }
 App.prototype = {
+  handleResize: function () {
+    let boundingRect = this.canvasEl.getBoundingClientRect();
+    this.width = boundingRect.width;
+    this.height = boundingRect.height;
+  },
+
   queueRender: function () {
     let that = this;
     requestAnimationFrame(function () {
@@ -13,7 +27,7 @@ App.prototype = {
     })
   },
   render: function () {
-    this.context.clearRect(0, 0, 100, 100);
+    this.context.clearRect(0, 0, this.width, this.height);
     this.context.fillRect(0, 0, 100, 100);
   }
 }
