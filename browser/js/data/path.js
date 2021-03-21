ed6fa06527e4c63e7cb79a2c07293d03eede62f4
Todd Wolfson <todd@twolfson.com>
2021-03-20 19:49:41 -0700

    Started to move to paths model

---
+++ browser/js/data/path.js
@@ -0,0 +1,18 @@
+// Load in our dependencies
+const utils = require('./utils');
+
+// Define our constructor
+function Path() {
+  this.vertexIdCount = 0;
+  this.vertexIds = new Uint8Array(0);
+}
+Path.prototype = {
+  addVertexId: function (id) {
+    this.vertexIds = utils.resizeArray(this.vertexIds, (this.vertexIdCount + 1) * 1);
+    this.vertexIds[this.vertexIdCount] = id;
+    this.vertexIdCount += 1;
+  },
+};
+
+// Export our constructor
+module.exports = Path;
--- browser/js/data/vector-network.js
+++ browser/js/data/vector-network.js
@@ -1,3 +1,6 @@
+// Load in our dependencies
+const utils = require('./utils');
+
 // Define our constructor
 function VectorNetwork() {
   this.verticesCount = 0;
@@ -5,28 +8,6 @@ function VectorNetwork() {
   this.paths = [];
 }
 VectorNetwork.prototype = {
-  // Based on: https://github.com/stephomi/sculptgl/blob/75bc19bfb666032c9c46010688a802502fc48fd9/src/mesh/dynamic/MeshDynamic.js#L190-L227
-  resizeArray: function (original, targetLength) {
-    // If our orginal is the same length as the target, then return the original
-    if (original.length === targetLength) {
-      return original;
-    }
-
-    // Otherwise, release subsection of original
-    if (original.length > targetLength) {
-      // DEV: If we grab *2 of the original and it exceeds the length, then it stops at the length as-is
-      //   but we like `Math.min` as extra protection
-      return original.subarray(0, Math.min(original.length, targetLength * 2));
-    }
-
-    // Otherwise, create new array to use
-    // DEV: We allocate 2x size to prevent resizing too often
-    //   However, as a result we must use `verticesCount` instead of the array length always
-    let tmp = new original.constructor(targetLength * 2);
-    tmp.set(original);
-    return tmp;
-  },
-
   getNearbyVertexId: function (x, y, minimumDistance) {
     // Define our default variables
     let closestVertexId = null;
@@ -51,7 +32,7 @@ VectorNetwork.prototype = {
   addVertex: function (x, y) {
     // Resize our data to handle the new element
     let vertexId = this.verticesCount;
-    this.vertices = this.resizeArray(this.vertices, (this.verticesCount + 1) * 2);
+    this.vertices = utils.resizeArray(this.vertices, (this.verticesCount + 1) * 2);
 
     // Update our new location
     this.vertices[vertexId*2 + 0] = x;
