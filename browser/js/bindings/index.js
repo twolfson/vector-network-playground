01adbef7471656e4f74e221430dc6dca2ea26c4e
Todd Wolfson <todd@twolfson.com>
2021-03-20 18:35:42 -0700

    Removed dev vertices

--- browser/js/bindings/index.js
+++ browser/js/bindings/index.js
@@ -5,17 +5,6 @@ function Bindings({ canvasEl, data }) {
 
   // Bind our listeners
   this._teardownFns = this.addEventListeners();
-
-  // TODO: Remove dev vertices
-  let vectorNetwork = this.data.vectorNetworks[0];
-  setTimeout(() => vectorNetwork.addVertex(0, 20), 1e3);
-  setTimeout(() => vectorNetwork.addVertex(40, 20), 3e3);
-  setTimeout(() => vectorNetwork.addVertex(80, 20), 5e3);
-  setTimeout(() => vectorNetwork.addVertex(120, 20), 7e3);
-  setTimeout(() => vectorNetwork.addVertex(160, 20), 9e3);
-  setTimeout(() => vectorNetwork.addVertex(200, 20), 11e3);
-  setTimeout(() => vectorNetwork.addVertex(240, 20), 13e3);
-  setTimeout(() => vectorNetwork.addVertex(280, 20), 15e3);
 }
 Bindings.prototype = {
   getMousePosition: function (evt) {
