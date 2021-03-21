0b23d637619e386ade216faa8e44f510176db8cb
Todd Wolfson <todd@twolfson.com>
2021-03-20 18:32:43 -0700

    Got clicking working

--- browser/js/bindings/index.js
+++ browser/js/bindings/index.js
@@ -18,6 +18,23 @@ function Bindings({ canvasEl, data }) {
   setTimeout(() => vectorNetwork.addVertex(280, 20), 15e3);
 }
 Bindings.prototype = {
+  getMousePosition: function (evt) {
+    // https://github.com/twolfson/blueprint3d/blob/twolfson-1.6.0/src/floorplanner/floorplanner.ts#L244-L249
+    let canvasOffset = this.getCanvasOffset();
+    return {
+      x: evt.pageX - canvasOffset.left,
+      y: evt.pageY - canvasOffset.top,
+    }
+  },
+  getCanvasOffset: function () {
+    // http://youmightnotneedjquery.com/#offset
+    let boundingRect = this.canvasEl.getBoundingClientRect();
+    return {
+      top: boundingRect.top + document.body.scrollTop,
+      left: boundingRect.left + document.body.scrollLeft,
+    };
+   },
+
   _addEventListener: function (eventType, fn) {
     let boundFn = fn.bind(this);
     this.canvasEl.addEventListener(eventType, boundFn);
@@ -32,7 +49,9 @@ Bindings.prototype = {
   },
 
   handleClick: function (evt) {
-    console.log('hi', evt);
+    let mouse = this.getMousePosition(evt);
+    let vectorNetwork = this.data.vectorNetworks[0];
+    vectorNetwork.addVertex(mouse.x, mouse.y);
   }
 }
 module.exports = Bindings;
