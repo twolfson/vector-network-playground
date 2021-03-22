b499dd93cd265aa9105d681fbe5c577712d6950c
Todd Wolfson <todd@twolfson.com>
2021-03-21 19:43:18 -0700

    Adjusted expectations but something is off for acute angles

--- browser/js/data/vector-network.js
+++ browser/js/data/vector-network.js
@@ -45,7 +45,7 @@ VectorNetwork.getAngle = function (vertexA, vertexB, vertexC) {
   // Perform our calculation
   vectorU.set(vertexA, false /* notify */); vectorU.subtract(vertexB, false /* returnNew */);
   vectorV.set(vertexC, false /* notify */); vectorU.subtract(vertexB, false /* returnNew */);
-  let angle = -vectorU.angleTo(vectorV);
+  let angle = vectorU.angleTo(vectorV);
   console.log('aaa', angle);
   if (angle < 0) { angle += Math.PI * 2; }
   console.log('bbb', angle);
--- test/data/vector-network--getAngle.js
+++ test/data/vector-network--getAngle.js
@@ -12,9 +12,9 @@ describe.only('VectorNetwork#getAngle', function () {
     B-A
     */
     let angle = VectorNetwork.getAngle(
-      new Vertex(0, 10),
-      new Vertex(0, 0),
       new Vertex(10, 0),
+      new Vertex(0, 0),
+      new Vertex(0, 10),
     );
     expect(angle).to.equal(Math.PI / 2);
   });
@@ -31,7 +31,7 @@ describe.only('VectorNetwork#getAngle', function () {
     expect(angle).to.equal(Math.PI);
   });
 
-  it('resolves internal angle for 270 degree angles', function () {
+  it('resolves 270 degree angles', function () {
     /*
     B-A
     |
@@ -42,11 +42,61 @@ describe.only('VectorNetwork#getAngle', function () {
       new Vertex(0, 0),
       new Vertex(0, -10),
     );
+    expect(angle).to.equal(3 * Math.PI / 2);
+  });
+
+  it('resolves 90 degree angles (negative start)', function () {
+    /*
+      C
+      |
+    A-B
+    */
+    let angle = VectorNetwork.getAngle(
+      new Vertex(0, -10),
+      new Vertex(0, 0),
+      new Vertex(10, 0),
+    );
+    expect(angle).to.equal(Math.PI / 2);
+  });
+
+  it('resolves 180 degree angles (negative start)', function () {
+    /*
+    A-B-C
+    */
+    let angle = VectorNetwork.getAngle(
+      new Vertex(-10, 0),
+      new Vertex(0,   0),
+      new Vertex(10,  0),
+    );
+    expect(angle).to.equal(Math.PI);
+  });
+
+  it('resolves 270 degree angles (negative start)', function () {
+    /*
+    A-B
+      |
+      C
+    */
+    let angle = VectorNetwork.getAngle(
+      new Vertex(-10,  0),
+      new Vertex(0,    0),
+      new Vertex(0,  -10),
+    );
     expect(angle).to.equal(Math.PI / 2);
   });
 
   it('resolves acute angles', function () {
-
+    /*
+      C
+     /
+    B-A
+    */
+    let angle = VectorNetwork.getAngle(
+      new Vertex(1, 0),
+      new Vertex(0, 0),
+      new Vertex(0, Math.sqrt(2)),
+    );
+    expect(angle).to.equal(Math.PI / 4);
   });
 
   it('resolves obtuse angles', function () {
